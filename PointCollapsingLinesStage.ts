cb68d3fae4e709a783771e16c1d33313f69aecf0
Anwesh43 <anweshthecool0@gmail.com>
2019-06-22 03:18:15 +0530

    adding PointCollapsingLineStage class

--- PointCollapsingLinesStage.ts
+++ PointCollapsingLinesStage.ts
@@ -65,3 +65,34 @@ class DrawingUtil {
         }
     }
 }
+
+class PointCollapsingLineStage {
+
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : PointCollapsingLineStage = new PointCollapsingLineStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
