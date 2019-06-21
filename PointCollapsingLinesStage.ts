f6a030ccdb6cfb1e301db552761040c157be161b
Anwesh43 <anweshthecool0@gmail.com>
2019-06-22 03:26:21 +0530

    adding PointCollaspingLine class

--- PointCollapsingLinesStage.ts
+++ PointCollapsingLinesStage.ts
@@ -185,3 +185,26 @@ class PCLNode {
         return this
     }
 }
+
+class PointCollapsingLine {
+    root : PCLNode = new PCLNode(0)
+    curr : PCLNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
